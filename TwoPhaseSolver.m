1c869a28e3f563fb7ae8610f8ebc7b9775b80cd9
Jason Chan <jasonjan2003@gmail.com>
2023-02-17 16:29:36 -0600

    valid classname

--- TwoPhaseSolver.m
+++ TwoPhaseSolver.m
@@ -1,7 +1,18 @@
-classdef 2PhaseSolver < handle
+classdef TwoPhaseSolver
+    %TWOPHASESOLVER A two-phase flow simulation program
+    % TODO detailed summary goes here
 
+    properties
+        
+    end
 
-
-
+    methods
+        
+        function obj = TwoPhaseSolver()
+            %TWOPHASESOLVER Construct and instance of this class
+            %   
+        end
+    
+    end
 
 end
