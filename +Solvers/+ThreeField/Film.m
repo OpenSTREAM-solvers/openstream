8729f19affe1b3627253df85913112041a80faa2
lecorrejm <lecorrejm@gmail.com>
2023-04-13 17:50:58 +0200

    test

--- +Solvers/+ThreeField/Film.m
+++ +Solvers/+ThreeField/Film.m
@@ -5,7 +5,6 @@ classdef Film < matlab.mixin.Copyable
      properties %(SetAccess=?Solvers.ThreeField.ThreeFieldSolver)
         
         % Solver properties
-
         NZ           (1,1) double  {mustBeNumeric}                         = 0                    % [-] Number of axial steps
         NTIME        (1,1) double  {mustBeNumeric}                         = 0                    % [-] Number of time steps
         TIME         (1,1) double  {mustBeNumeric}                         = 0                    % [s] Time series
@@ -31,7 +30,6 @@ classdef Film < matlab.mixin.Copyable
         inputSet                   {isa(inputSet,'Inputs.InputSet')}
         fluid                      {isa(fluid,'Inputs.FluidProperties')}
      end
-    
     
 
     methods
