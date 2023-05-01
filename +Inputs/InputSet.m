521c55d96a512a25d4d3dba91bfed704ea694fba
RWMorse <rmorse@wisc.edu>
2023-04-30 20:04:17 -0500

    fix: typo in getReport

--- +Inputs/InputSet.m
+++ +Inputs/InputSet.m
@@ -75,7 +75,7 @@ classdef InputSet
                 obj.geometry = Geometry(opts.geometryFilePath, opts.geometryID);
                 obj.bc = BoundaryConditions(opts.bcFilePath, obj.geometry);
             catch ME
-                getreport(ME)
+                getReport(ME)
                 obj.session.log.toggleDiary();
             end
             obj.session.log.toggleDiary();
