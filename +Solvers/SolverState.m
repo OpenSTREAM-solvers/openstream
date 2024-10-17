b5a823faeda339e5ee61fb6d7de2d576a3345b50
Jason Chan <jasonjan2003@gmail.com>
2024-10-17 11:00:17 -0500

    refactor: reorganized SolverStates

--- +Solvers/SolverState.m
+++ +Solvers/SolverState.m
@@ -2,11 +2,14 @@ classdef SolverState < uint16
 %SOLVERSTATE Enumeration class of possible solver states
 %
     enumeration
-        UNSOLVED                    (0)
+        UNSOLVED                    (8)
+        CREATED                     (0)
         SOLVEDCONVERGED             (1)
         SOLVEDNOTCONVERGED          (2)
         INITIALSTEPCONVERGED        (3)
         INITIALSTEPNOTCONVERGED     (4)
-        INITIALIZED                 (5)
+        UNINITIALIZED               (5)
+        INITIALIZED                 (6)
+        EMPTY                       (7)
     end
 end
