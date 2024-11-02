a1154ba68bff7a1dae51a7898b8a78e12cb9c5eb
Jason Chan <jasonjan2003@gmail.com>
2024-11-01 20:42:12 -0500

    fix: split obs w ratio to 50-50

--- +Inputs/Model.m
+++ +Inputs/Model.m
@@ -57,7 +57,7 @@ classdef Model < Inputs.Input
         WAVEMIXCOEF     (:,1) double  {mustBeNonnegative}                  = 0.0                   % Wave mixing coefficient
         
         % Obstruction Solver Only
-        OBSWSPLITRATIO  (2,1) double {mustBeNonnegative}                   = [0.0275 0.001]        % Split ratio of mass downstream of obstruction (2nd term is the wake region) [kg/s]
+        OBSWSPLITRATIO  (2,1) double {mustBeNonnegative}                   = [0.0266 0.001] .* [0.5 0.5]        % Split ratio of mass downstream of obstruction (2nd term is the wake region) [kg/s]
     end
 
     properties (Constant)
---
+++ sandbox/ObsTest_default.m
@@ -0,0 +1,67 @@
+inputSet_noObs = Inputs.InputSet( ...
+                    modelFilePath = './inputs/models.inp', modelID = 'BARCS', ...
+                    optionsFilePath = './inputs/options.inp', optionsID = 'STEADY', ...
+                    geometryFilePath = './inputs/geom.inp', geometryID = 'BARC', ...
+                    bcFilePath = './inputs/bc_barc.inp', ...
+                    sessionParentDir = fullfile(pwd,'outputs'), ...
+                    overwriteSessionFiles = true, ...
+                    LOGMODE = 'BOTH');
+
+solver = Solvers.ThreeField.ThreeFieldSolver(inputSet_noObs);
+% solver = Solvers.FourField.FourFieldSolver(inputSet_noObs);
+
+solver.solve();
+
+%% PLOT?
+solver.mixSolver.plotz(1);
+%tfSolver.mixSolver.plotz(tfSolver.mixSolver.NTIME);
+% tfSolver.plotz(1);
+linkaxes(findobj(gcf().Children.Children,'Type','Axes'),'x');
+%xlim([0, 2.75]);
+solver.plotz(1);
+sgtitle('Non-obstructed')
+
+%% OBS Solver
+
+inputSet = Inputs.InputSet( ...
+    modelFilePath = './inputs/models.inp', modelID = 'BARCS', ...
+    optionsFilePath = './inputs/options.inp', optionsID = 'STEADY', ...
+    geometryFilePath = './inputs/geom.inp', geometryID = 'BARC', ...
+    bcFilePath = './inputs/bc_barc.inp', ...
+    obsFilePath = './inputs/obstruction.inp', obsIDs = {'ROD'}, ...
+    sessionParentDir = fullfile(pwd,'outputs'), ...
+    overwriteSessionFiles = true, ...
+    LOGMODE = 'BOTH');
+obsSolver = Solvers.Obstruction.ObstructionSolver(inputSet, 'THREEFIELD');
+% obsSolver = Solvers.Obstruction.ObstructionSolver(inputSet, 'FOURFIELD');
+obsSolver.solve();
+
+% Plot
+plotter = obsSolver.solutionSets(1).solver.mixSolver.plotz(1);
+% obsSolver.solutionSets(1).solver.mixSolver.plotz(obsSolver.solutionSets(1).solver.mixSolver.NTIME);
+% obsSolver.solutionSets(1).solver.plotz(1);
+linkaxes(findobj(gcf().Children.Children,'Type','Axes'),'x');
+
+plotter = obsSolver.solutionSets(2).solver.mixSolver.plotz(1, 'plotter', plotter);
+% obsSolver.solutionSets(1).solver.mixSolver.plotz(obsSolver.solutionSets(1).solver.mixSolver.NTIME);
+xlim(plotter.gca(), 'auto');
+ylim(plotter.gca(), 'auto');
+
+plotter = obsSolver.solutionSets(3).solver.mixSolver.plotz(1, 'plotter', plotter);
+% obsSolver.solutionSets(1).solver.mixSolver.plotz(obsSolver.solutionSets(1).solver.mixSolver.NTIME);
+linkaxes(findobj(findobj(gcf().Children, 'Type', 'tiledlayout').Children,'Type','Axes'),'x');
+
+
+
+plotter = obsSolver.solutionSets(1).solver.plotz(1);
+linkaxes(findobj(findobj(gcf().Children, 'Type', 'tiledlayout').Children,'Type','Axes'),'x');
+xlim([0 5.5]);
+
+plotter = obsSolver.solutionSets(2).solver.plotz(1, 'plotter', plotter);
+linkaxes(findobj(findobj(gcf().Children, 'Type', 'tiledlayout').Children,'Type','Axes'),'x');
+xlim([0 5.5]);
+sgtitle('three-field, obstructed, pre-wake regions')
+
+plotter = obsSolver.solutionSets(3).solver.plotz(1, 'plotter', plotter);
+linkaxes(findobj(findobj(gcf().Children, 'Type', 'tiledlayout').Children,'Type','Axes'),'x');
+xlim([0 5.5]);
