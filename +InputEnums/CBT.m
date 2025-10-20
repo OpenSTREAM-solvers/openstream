3be0249fcce5f090a57be1e79344e9cfcaa7cbd0
cjdunbar <cjdunbar@ad.engr.wisc.edu>
2025-10-20 13:04:17 -0500

    feat(mixture solver): add Groeneveld LUT for CBT
    This implements an option for the user to select the 2006 Groeneveld LUT for
    calculating where in the channel critical heat flux occurs. It includes K1
    and K4 factors. Future updates may address storing the LUT data locally
    so that is need not be read into memory each time the function is
    called.

--- +InputEnums/CBT.m
+++ +InputEnums/CBT.m
@@ -7,6 +7,7 @@ classdef CBT
     enumeration
         NONE                 % No CBT transition
         BIASI                % Biasi model
+        LUT2006              % Groeneveld 2006 LUT
     end
 
 end
