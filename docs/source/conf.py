7f2ae08371d7d7c569b67fa7c6b8cdaf4670f949
lecorrejm <lecorrejm@gmail.com>
2025-11-08 16:59:24 +0100

    docs(Mixture): Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ extensions = [
     'sphinxcontrib.matlab',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon', 
     'sphinx_rtd_theme',
     'sphinx_copybutton',
