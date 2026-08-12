56d04872a56d02207cb852afb26a68364fa8a570
Jason Chan <jasonjan2003@gmail.com>
2026-08-12 15:56:04 -0500

    ci: set up inputfiles

--- tests/SolverTest.m
+++ tests/SolverTest.m
@@ -1,11 +1,15 @@
 classdef SolverTest < matlab.unittest.TestCase
 
-    properties (TestParameter)
-
-        
-
-    end
+    % properties (TestParameter)
+    % 
+    %     testInputFiles
+    % 
+    % end
     
+    properties
+        testInputFile
+    end
+
     methods (TestClassSetup)
 
         % Shared setup for the entire test class
@@ -14,7 +18,10 @@ classdef SolverTest < matlab.unittest.TestCase
             % Currently assumes openstream-database is in parent directory
             %   TODO: improve this configuration setting. Maybe we can use
             %   some kind of configuration file... YAML?
+            %   TODO: create parameterized files
+            %
             addpath('../../openstream-database')
+            addpath('../../openstream')
 
             % Demo inputs
             entryData = table;
@@ -28,6 +35,15 @@ classdef SolverTest < matlab.unittest.TestCase
             entryData.Area          = 6.0821e-05;                              % [m^2]  Coolant area
             entryData.Length        = 5.5;                                     % [m]    Length
             entryData.Fluid         = 'water';                                 %        Fluid
+            
+            % Create a Dataset
+            ds = Dataset(1,'isLightWeight',true,'lightWeightEntryData',entryData);
+            
+            % inputOptions
+            opts = ds.inputOptions();
+            opts.options.AXIALINTERP = 'NEXT';                                 % Axial power interpolation method
+
+            testCase.testInputFile = ds.makeInputFiles(opts);
 
         end
     end
@@ -42,6 +58,13 @@ classdef SolverTest < matlab.unittest.TestCase
         function solverTest(testCase)
             testCase.verifyFail("Unimplemented test");
         end
+    end
+
+    methods (TestClassTeardown)
+        function removeInputOutputFiles(testCase)
+            rmdir('./+*','s');
+        end
+
     end
 
 end
